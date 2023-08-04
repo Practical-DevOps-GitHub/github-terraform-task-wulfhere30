47127c5cf10c7a0901706801814d3534dffe58fa
Ruslan Akhrimov <56388440+wulfhere30@users.noreply.github.com>
2023-08-04 16:35:44 +0300

    Update main.tf

--- .github/main.tf
+++ .github/main.tf
@@ -1,21 +1,21 @@
 resource "github_repository_collaborator" "softservedata" {
-  repository = "github-terraform-task-wulfhere30"
+  repository = "Practical-DevOps-GitHub/github-terraform-task-wulfhere30"
   username   = "softservedata"
   permission = "push"
 }
 
 resource "github_branch" "develop" {
-  repository = "github-terraform-task-wulfhere30"
+  repository = "Practical-DevOps-GitHub/github-terraform-task-wulfhere30"
   branch     = "develop"
 }
 
 resource "github_branch_default" "develop" {
-  repository = "github-terraform-task-wulfhere30"
+  repository = "Practical-DevOps-GitHub/github-terraform-task-wulfhere30"
   branch     = github_branch.develop.branch
 }
 
 resource "github_branch_protection" "main" {
-  repository_id  = "github-terraform-task-wulfhere30"
+  repository_id  = "Practical-DevOps-GitHub/github-terraform-task-wulfhere30"
   pattern        = "main"
   enforce_admins = true
 
@@ -27,7 +27,7 @@ resource "github_branch_protection" "main" {
 }
 
 resource "github_branch_protection" "develop" {
-  repository_id  = "github-terraform-task-wulfhere30"
+  repository_id  = "Practical-DevOps-GitHub/github-terraform-task-wulfhere30"
   pattern        = github_branch.develop.branch
   enforce_admins = true
 
@@ -39,22 +39,22 @@ resource "github_branch_protection" "develop" {
 }
 
 resource "github_repository_file" "pull_request_template" {
-  repository = "github-terraform-task-wulfhere30"
+  repository = "Practical-DevOps-GitHub/github-terraform-task-wulfhere30"
   file       = ".github/pull_request_template.md"
   content    = <<EOF
-##Describe your changes
-##Issue ticket number and link
-##Checklist before requesting a review
- - [] I have performed a self-review of my code
- - [] If it is a core feature, I have added thorough tests
- - [] Do we need to implement analytics?
- - [] Will this be part of a product update? If yes, please write one phrase about this update
+Describe your changes
+Issue ticket number and link
+Checklist before requesting a review
+ - I have performed a self-review of my code
+ - If it is a core feature, I have added thorough tests
+ - Do we need to implement analytics?
+ - Will this be part of a product update? If yes, please write one phrase about this update
 EOF
 }
 
 resource "github_repository_deploy_key" "deploy_key" {
   title      = "DEPLOY_KEY"
-  repository = "github-terraform-task-wulfhere30"
+  repository = "Practical-DevOps-GitHub/github-terraform-task-wulfhere30"
   key        = var.deploy_key_public_key
 }
 
@@ -63,3 +63,4 @@ resource "github_actions_secret" "pat_secret" {
   secret_name     = "PAT"
   plaintext_value = var.pat_token
 }
+
