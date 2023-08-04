7e56f5bea9f45ab6435e3638f6ad334ea490afe3
Ruslan Akhrimov <56388440+wulfhere30@users.noreply.github.com>
2023-08-04 16:13:38 +0300

    Update main.tf

--- .github/main.tf
+++ .github/main.tf
@@ -1,53 +1,16 @@
 provider "github" {
-  organization = "my-organization"
+  organization = "github-terraform-task-wulfhere30"
   token = var.github_token
 }
 
-resource "github_repository" "my-repository" {
-  name = "my-repository"
+resource "github_repository" "github-terraform-task-wulfhere30" {
+  name = "github-terraform-task-wulfhere30"
   description = "My repository"
-  homepage = "https://github.com/my-organization/my-repository"
+  homepage = "https://github.com/Practical-DevOps-GitHub/github-terraform-task-wulfhere30"
   visibility = "public"
 
   collaborator {
     username = "softservedata"
+    role = "collaborator"
   }
-
-  default_branch = "develop"
-
-  branch_protection {
-    branch = "main"
-    required_pull_request_reviews = 2
-    allow_merge_on_conflict = false
-    code_owner_reviews_required = true
-  }
-
-  branch_protection {
-    branch = "develop"
-    required_pull_request_reviews = 1
-    allow_merge_on_conflict = false
-  }
-
-  deploy_key {
-    name = "DEPLOY_KEY"
-    read_only = false
-  }
-
-  webhook {
-    url = "https://my-discord-server/webhook"
-    events = ["pull_request"]
-  }
-
-  action_secret {
-    name = "PAT"
-    value = var.github_pat
-  }
-}
-
-variable "github_token" {
-  type = string
-}
-
-variable "github_pat" {
-  type = string
 }
